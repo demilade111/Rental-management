282b2be9ee2f9acf7e8534c729088faf82de7dec
demilade111 <64733187+demilade111@users.noreply.github.com>
2025-09-24 13:08:37 -0700

    Add client and server applications with Docker configuration

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <main className='bg-blue-500'>
+      <div >
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+   
+    </main>
+  )
+}
+
+export default App
