05a05dd8642943f68c3a14350d01230f125d6665
Muco Abu <mbekere1@outlook.com>
2025-10-04 15:57:04 -0700

    added controller request reset and reset password

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,4 +1,4 @@
-import { registerUser, loginUser } from "../services/authService.js";
+import { registerUser, loginUser, requestPasswordReset, resetPassword } from "../services/authService.js";
 
 export const register = async (req, res) => {
     try {
@@ -21,5 +21,35 @@ export const login = async (req, res) => {
         return res.status(200).json({ message: "Login successful", token, user });
     } catch (error) {
         return res.status(400).json({ error: error.message });
+    }
+};
+
+//Controller for requesting password reset
+export const requestReset = async (req, res) => {
+    try {
+        const { email } = req.body;
+        if (!email) return res.status(400).json({ error: "Email is required" });
+
+        const token = await requestPasswordReset(email);
+        res.json({ message: "Password reset token generated", token });
+    } catch (err) {
+
+        return res.status(400).json({ error: err.message });
+        
+    }
+     };
+
+//Controller for resetting password using the token
+export const resetController = async (req, res) => {
+    try {
+        const { token, newPassword } = req.body;
+        if (!token || !newPassword) {
+            return res.status(400).json({ error: "Token and new password are required" });
+        }   
+
+        await resetPassword(token, newPassword);
+        res.json({ message: "Password has been reset successfully" });
+    } catch (err) {
+        return res.status(400).json({ error: err.message });
     }
 };
