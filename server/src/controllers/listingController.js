8faab56f3b65791eb3c439556e0760b8bbe34dad
demilade111 <64733187+demilade111@users.noreply.github.com>
2025-10-09 23:09:43 -0700

    feat(listings): add fetching functionality for listings
    - Implemented `fetchAllListings` and `fetchListingById` functions in `listingController.js` to retrieve all listings and a specific listing by ID.
    - Updated `listingService.js` to include the new service methods for fetching listings from the database.
    - Modified `listingRoute.js` to add corresponding GET endpoints for fetching listings, enhancing the API's listing management capabilities.

--- server/src/controllers/listingController.js
+++ server/src/controllers/listingController.js
@@ -1,5 +1,9 @@
 import { createListingSchema } from "../validations/listingValidation.js";
-import { createListings } from "../services/listingService.js";
+import {
+  createListings,
+  getAllListings,
+  getListingById,
+} from "../services/listingService.js";
 
 async function createListing(req, res, next) {
   try {
@@ -17,4 +21,30 @@ async function createListing(req, res, next) {
   }
 }
 
-export { createListing };
+async function fetchAllListings(req, res, next) {
+  try {
+    const listings = await getAllListings();
+    res.status(200).json({
+      success: true,
+      message: "Listings fetched successfully",
+      data: listings,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+async function fetchListingById(req, res, next) {
+  try {
+    const { id } = req.params;
+    const listing = await getListingById(id);
+    res.status(200).json({
+      success: true,
+      message: "Listing fetched successfully",
+      data: listing,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export { createListing, fetchAllListings, fetchListingById };
