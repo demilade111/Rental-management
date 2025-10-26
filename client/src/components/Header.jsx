9440719952e6928d005a2fd87af36478e90cc911
Zin Min Htun <zinminhtunzmh11@gmail.com>
2025-10-26 01:22:38 -0700

    conditionally rendering the greeting message in header only when the url is on dashboard.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,27 @@
-const Header = ({ user, isLandlord }) => (
+import { useLocation } from "react-router-dom";
+
+const Header = ({ user, isLandlord }) => {
+  const location = useLocation();
+  const showGreeting = location.pathname === "/landlord/dashboard";
+
+  return (
     <div className="bg-white px-8 py-6 flex justify-between items-center">
+      {showGreeting ? (
         <h2 className="text-[16px] text-gray-900">
-            Good morning, <span className="font-semibold">{user?.firstName} {user?.lastName} !</span>
+          Good morning,{" "}
+          <span className="font-semibold">
+            {user?.firstName} {user?.lastName}!
+          </span>
         </h2>
-        <span className="text-sm text-gray-600">
-            {isLandlord ? 'Noti' : 'Noti'}
-        </span>
+      ) : (
+        <div />
+      )}
+
+      <span className="text-sm text-gray-600">
+        {isLandlord ? "Noti" : "Noti"}
+      </span>
     </div>
-);
+  );
+};
 
 export default Header;
